9fd08f135d11d2aa31e1f8963ff5324434cad611
shrestharonit09 <stharonit09@gmail.com>
2024-09-01 17:13:04 +0545

    card done

---
+++ src/component/Card.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Logo from "../Image/TUlogo.png";
+import Barcode from "../Image/barcode.png";
+
+const Card = (props) => {
+  return (
+    <div className="w-1/4">
+      <div className="bg-gradient-to-b from-blue-100 via-white to-blue-300 rounded-lg shadow-lg flex flex-col gap-2">
+        <div className="flex gap-8 m-2">
+          <div>
+            <h1 className="font-bold text-2xl text-white px-2 py-4 bg-blue-700 rounded-lg">
+              B.Sc.CSIT
+            </h1>
+          </div>
+
+          <img className="h-20 w-20" src={Logo} alt="TU LOGO" />
+        </div>
+        <div>
+          <h1 className="font-semibold text-sm text-center bg-gradient-to-r from-blue-800 to-sky-800 bg-clip-text text-transparent ">
+            TRIBHUVAN UNIVERSITY
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold text-red-600 text-center">
+            BHAKTAPUR MULTIPLE CAMPUS
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-xs text-center font-bold">
+            DOODHPATI-17, BHAKTAPUR :+977-1-6610200/6613199
+          </h1>
+        </div>
+        <div className="flex justify-center">
+          <img className="w-32 h-32 " src={Logo} alt="photo" />
+        </div>
+        <div className="flex">
+          <div className="flex flex-col px-12 gap-2 text-blue-900 font-semibold">
+            <label htmlFor="ID">ID</label>
+            <label htmlFor="Name">Name</label>
+            <label htmlFor="Address">Address</label>
+            <label htmlFor="Age">Age</label>
+          </div>
+          <div className="flex flex-col gap-2 text-blue-900 font-semibold">
+            <h1>: {props.ID}</h1>
+            <h1>: {props.Name}</h1>
+            <h1>: {props.Address}</h1>
+            <h1>: {props.Age}</h1>
+            <img className="h-16 w-44" src={Barcode} alt="" />
+          </div>
+        </div>
+        <div className="bg-blue-600 rounded-b-lg">
+          <h1 className="text-2xl font-bold text-white text-center">
+            STUDENT IDENTITY CARD
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/component/Crudoperation.js
+++ src/component/Crudoperation.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import Card from "./Card";
 
-const Crudoperation= () => {
+const Crudoperation = () => {
   const [ims, setIMS] = useState({
     ID: "",
     Name: "",
@@ -10,13 +11,12 @@ const Crudoperation= () => {
 
   const [newdata, setNewdata] = useState([]);
   const [isEdit, setIsedit] = useState(false); // first ma submit dekhaune ..
+  const [cards, setCard] = useState();
 
   const handleClicked = () => {
     const { ID, Name, Address, Age } = ims; //destructing methods that takes the value of ims as a object data
-
     if (ID && Address && Age && Name) {
       if (newdata.some((items) => items.ID === ID)) {
-        //to validate if previous ID is already exist
         alert("ID Already Exist, ID Should be Unique");
       } else {
         setNewdata([...newdata, ims]);
@@ -33,7 +33,7 @@ const Crudoperation= () => {
   };
   const handleEdit = (id) => {
     const edit = newdata.find((value) => value.ID === id);
-    if (edit!==undefined) {
+    if (edit !== undefined) {
       setIMS({
         ID: edit.ID,
         Name: edit.Name,
@@ -41,145 +41,162 @@ const Crudoperation= () => {
         Age: edit.Age,
       });
     }
-    setNewdata([ims]);
+    setNewdata((previousdata) =>
+      previousdata.map((item) => (item.ID == ims.ID ? ims : item))
+    );
     setIsedit(true);
   };
   const handleUpdateEdit = () => {
     handleEdit();
     setIsedit(false);
-    setIMS({
-      ID: "",
-      Name: "",
-      Address: "",
-      Age: "",
-    });
   };
   const handleDelete = (id) => {
     const deletes = newdata.filter((value) => value.ID !== id);
     setNewdata(deletes);
   };
+  const handleView = (id) => {
+    const user = newdata.find((value) => value.ID === id);
+    setCard(user);
+  };
+
   return (
-    <div className="flex justify-around mt-4 ">
-      <div className="w-1/3">
-        <div className="flex flex-col gap-6 p-8 rounded-lg bg-blue-500 shadow-lg">
-          <h1 className="text-3xl bg-gradient-to-r from-yellow-400 via-orange-500 to-red-500 font-bold mb-4 bg-clip-text text-transparent text-center">
-            Information Management System
-          </h1>
-          <label className=" text-xl font-semibold text-white" htmlFor="id">
-            ID
-          </label>
-          <input
-            className="rounded-lg h-8 px-2"
-            value={ims.ID}
-            onChange={(event) => setIMS({ ...ims, ID: event.target.value })}
-            placeholder="ID"
-            type="number"
-            required
-          />
+    <div>
+      <div className="flex justify-around mt-4 ">
+        <div className="w-1/3">
+          <div className="flex flex-col gap-6 p-8 rounded-lg bg-blue-500 shadow-lg">
+            <h1 className="text-3xl bg-gradient-to-r from-yellow-400 via-orange-500 to-red-500 font-bold mb-4 bg-clip-text text-transparent text-center">
+              Information Management System
+            </h1>
+            <label className=" text-xl font-semibold text-white" htmlFor="id">
+              ID
+            </label>
+            <input
+              className="rounded-lg h-8 px-2"
+              value={ims.ID}
+              onChange={(event) => setIMS({ ...ims, ID: event.target.value })}
+              placeholder="ID"
+              type="number"
+              required
+            />
 
-          <label className=" text-xl font-semibold text-white" htmlFor="Name">
-            Name
-          </label>
-          <input
-            className="rounded-lg h-8 px-2"
-            value={ims.Name}
-            onChange={(event) => setIMS({ ...ims, Name: event.target.value })}
-            placeholder="Name"
-            type="text"
-            required
-          />
+            <label className=" text-xl font-semibold text-white" htmlFor="Name">
+              Name
+            </label>
+            <input
+              className="rounded-lg h-8 px-2"
+              value={ims.Name}
+              onChange={(event) => setIMS({ ...ims, Name: event.target.value })}
+              placeholder="Name"
+              type="text"
+              required
+            />
 
-          <label
-            className=" text-xl font-semibold text-white"
-            htmlFor="Address"
-          >
-            Address
-          </label>
-          <input
-            className="rounded-lg h-8 px-2"
-            value={ims.Address}
-            onChange={(event) =>
-              setIMS({ ...ims, Address: event.target.value })
-            }
-            placeholder="Address"
-            type="text"
-            required
-          />
+            <label
+              className=" text-xl font-semibold text-white"
+              htmlFor="Address"
+            >
+              Address
+            </label>
+            <input
+              className="rounded-lg h-8 px-2"
+              value={ims.Address}
+              onChange={(event) =>
+                setIMS({ ...ims, Address: event.target.value })
+              }
+              placeholder="Address"
+              type="text"
+              required
+            />
 
-          <label className=" text-xl font-semibold text-white" htmlFor="age">
-            Age
-          </label>
-          <input
-            className="rounded-lg h-8 px-2"
-            value={ims.Age}
-            onChange={(event) => setIMS({ ...ims, Age: event.target.value })}
-            placeholder="Age"
-            type="number"
-            required
-          />
-          <div className="flex justify-center">
-            {isEdit ? (
-              <button
-                className="rounded-lg bg-green-500 text-white font-semibold w-1/2 px-2 py-1 text-xl"
-                onClick={handleUpdateEdit}
-              >
-                UpdateEdit
-              </button>
-            ) : (
-              <button
-                className="rounded-lg bg-green-500 text-white font-semibold w-1/2 px-2 py-1 text-xl"
-                onClick={handleClicked}
-              >
-                Submit
-              </button>
-            )}
+            <label className=" text-xl font-semibold text-white" htmlFor="age">
+              Age
+            </label>
+            <input
+              className="rounded-lg h-8 px-2"
+              value={ims.Age}
+              onChange={(event) => setIMS({ ...ims, Age: event.target.value })}
+              placeholder="Age"
+              type="number"
+              required
+            />
+            <div className="flex justify-center">
+              {isEdit ? (
+                <button
+                  className="rounded-lg bg-green-500 text-white font-semibold w-1/2 px-2 py-1 text-xl"
+                  onClick={handleUpdateEdit}
+                >
+                  UpdateEdit
+                </button>
+              ) : (
+                <button
+                  className="rounded-lg bg-green-500 text-white font-semibold w-1/2 px-2 py-1 text-xl"
+                  onClick={handleClicked}
+                >
+                  Submit
+                </button>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-1/2">
-        <div className="bg-gray-200 shadow-lg border border-gray-300 w-full p-2">
-          <h1 className="text-center font-semibold text-xl">
-            List of register users
-          </h1>
-        </div>
+        <div className="w-1/2">
+          <div className="bg-gray-200 shadow-lg border border-gray-300 w-full p-2 flex justify-center">
+            <h1 className="font-semibold text-xl">List of register users</h1>
+          </div>
 
-        <table className="table-auto w-full">
-          <thead>
-            <tr className="border border-gray-200 text-lg">
-              <th className="text-left p-4">S.NO</th>
-              <th className="text-left p-4">ID</th>
-              <th className="text-left p-4">Name</th>
-              <th className="text-left p-4">Address</th>
-              <th className="text-left p-4">Age</th>
-              <th className="text-center p-4">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {newdata.map((items, index) => (
-              <tr className="border border-gray-200" key={index}>
-                <td className="text-lg text-left p-4">{index + 1}</td>
-                <td className="text-lg text-left p-4">{items.ID}</td>
-                <td className="text-lg text-left p-4">{items.Name}</td>
-                <td className="text-lg text-left p-4">{items.Address}</td>
-                <td className="text-lg text-left p-4">{items.Age}</td>
-                <td className="flex justify-around mt-2">
-                  <button
-                    className="bg-green-500 rounded-lg p-2 text-xl"
-                    onClick={() => handleEdit(items.ID)}
-                  >
-                    edit
-                  </button>
-                  <button
-                    className="bg-red-500 rounded-lg text-xl p-2"
-                    onClick={() => handleDelete(items.ID)}
-                  >
-                    delete
-                  </button>
-                </td>
+          <table className="table-auto w-full">
+            <thead>
+              <tr className="border border-gray-200 text-lg">
+                <th className="text-left p-4">S.NO</th>
+                <th className="text-left p-4">ID</th>
+                <th className="text-left p-4">Name</th>
+                <th className="text-left p-4">Address</th>
+                <th className="text-left p-4">Age</th>
+                <th className="text-center p-4">Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {newdata.map((items, index) => (
+                <tr className="border border-gray-200" key={index}>
+                  <td className="text-lg text-left p-4">{index + 1}</td>
+                  <td className="text-lg text-left p-4">{items.ID}</td>
+                  <td className="text-lg text-left p-4">{items.Name}</td>
+                  <td className="text-lg text-left p-4">{items.Address}</td>
+                  <td className="text-lg text-left p-4">{items.Age}</td>
+                  <td className="flex justify-around mt-2">
+                    <button
+                      className="bg-green-500 rounded-lg p-2 text-xl"
+                      onClick={() => handleEdit(items.ID)}
+                    >
+                      edit
+                    </button>
+                    <button
+                      className="bg-red-500 rounded-lg text-xl p-2"
+                      onClick={() => handleDelete(items.ID)}
+                    >
+                      delete
+                    </button>
+                    <button
+                      className="bg-blue-500 rounded-lg text-xl p-2"
+                      onClick={() => handleView(items.ID)}
+                    >
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div>
+        {cards && (
+          <Card
+            ID={cards?.ID ? cards.ID : "1"}
+            Name={cards?.Name ? cards.Name : "Ronit Shrestha"}
+            Address={cards?.Address ? cards.Address : "bhaktapur"}
+            Age={cards?.Age ? cards.Age : "24"}
+          />
+        )}
       </div>
     </div>
   );
